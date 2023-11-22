cdb44ef83c3c6a225545c42749a72bba51b41f12
Jiya <coderaholic21@gmail.com>
2023-11-23 01:10:27 +0530

    Updated

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -14,18 +14,18 @@ import home_icon from './Assets/home.png'
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      {/* Twitter icon */}
+      {/* Twit icon */}
       <img className='sidebar_twit_icon' width={'25px'} src={home_icon} alt="" />
 
       {/* Sidebar Options */}
       <SidebarOption active={true} text="Home" icon={home_icon}/>
-      <SidebarOption text="Explore" icon={home_icon}/>
-      <SidebarOption text="Events" icon={home_icon}/>
-      <SidebarOption text="Resources" icon={home_icon}/>
-      <SidebarOption text="Messages" icon={home_icon}/>
-      <SidebarOption text="Job Mela" icon={home_icon}/>
-      <SidebarOption text="Donate" icon={home_icon}/>
-      <SidebarOption text="Profile" icon={home_icon}/>
+      <SidebarOption text="Explore" icon={"https://cdn-icons-png.flaticon.com/512/61/61088.png"}/>
+      <SidebarOption text="Events" icon={"https://cdn-icons-png.flaticon.com/512/3995/3995725.png"}/>
+      <SidebarOption text="Resources" icon={"https://cdn-icons-png.flaticon.com/512/751/751432.png"}/>
+      {/* <SidebarOption text="Messages" icon={home_icon}/> */}
+      <SidebarOption text="Job Mela" icon={"https://icon-library.com/images/jobs-icon-png/jobs-icon-png-18.jpg"}/>
+      <SidebarOption text="Donate" icon={"https://cdn-icons-png.flaticon.com/512/176/176696.png"}/>
+      {/* <SidebarOption text="Profile" icon={home_icon}/> */}
 
       <button className='sidebar_tweet'>Tweet</button>
     </div>
--- src/Widgets.js
+++ src/Widgets.js
@@ -3,9 +3,29 @@ import './Widgets.css'
 
 const Widgets = () => {
   return (
-    <div className='widgets'>
-      <h2>Widgets</h2>
+    <div className="widgets">
+    <div className="widgets__input">
+      {/* <SearchIcon className="widgets__searchIcon" /> */}
+      <input placeholder="Search" type="text" />
     </div>
+
+    <div className="widgets__widgetContainer">
+      <h2>What's happening</h2>
+
+      {/* <TwitTweetEmbed tweetId={"858551177860055040"} /> */}
+
+      {/* <TwitTimelineEmbed
+        sourceType="profile"
+        screenName="cleverqazi"
+        options={{ height: 400 }}
+      />
+
+      <TwitShareButton
+        url={"https://facebook.com/cleverprogrammer"}
+        options={{ text: "#reactjs is awesome", via: "cleverqazi" }}
+      /> */}
+    </div>
+  </div>
   )
 }
 
