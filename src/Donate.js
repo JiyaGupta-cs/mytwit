49b5162716bc1db1be908bd2855954fb636fc6ce
Jiya <coderaholic21@gmail.com>
2023-12-02 02:16:31 +0530

    Updated Donate Page Banner

--- src/Donate.js
+++ src/Donate.js
@@ -14,7 +14,42 @@ const Donate = () => {
             {/* <Sidebar />, <DonateBox />, <Widgets /> */}
             <Sidebar />
             <div className="donate_box">
-                {/* <h2>Support</h2> */}
+
+
+            {/* <img className='donatebanner' width={'820px'} height={'350px'} src={donate_banner} alt="" /> */}
+
+
+            <div class="container">
+  <h1 class="title">Donate</h1>
+  <h1 class="title title-large">Donate</h1>
+  <div id="img-1" class="img-container">   
+    <img class="img" src={"https://assets-global.website-files.com/63804561992fec607543f6d0/644a3675f3756f74bad835e9_Frame-2.png"}/>
+  </div>
+  
+  {/* <div class="img-container second-animation">    
+    <img class="img" src={"https://wallpapershome.com/images/pages/pic_v/13886.jpg"}/>
+  </div> */}
+  
+  <div class="img-container third-animation">    
+    <img class="img" src={"https://assets-global.website-files.com/63804561992fec607543f6d0/63d9cee2f1e5b9cc7415c707_Ganga%20color%20(home%20page).png"}/>
+  </div>
+  
+  <div class="img-container fourth-animation">    
+    <img class="img nba" src={"https://assets-global.website-files.com/63804561992fec607543f6d0/63eba6f7c2eaacf52c8fd46f_Frame.png"}/>
+  </div>
+
+  <div class="img-container fifth-animation">    
+    <img class="img" src={"https://assets-global.website-files.com/63804561992fec607543f6d0/644a3674bcbc64e39d8707d9_Frame.png"}/>
+  </div>  
+  
+  {/* <div id="img-6" class="img-container sixth-animation">    
+    <img class="img" src={"http://orig15.deviantart.net/3c71/f/2016/121/9/4/reaper_wallpaper__overwatch__by_prollgurke-da0wf9m.png"}/>
+  </div>   */}
+    
+  <div id="img-7" class="img-container seventh-animation">    
+    <img class="img" src={"https://assets-global.website-files.com/63804561992fec607543f6d0/63d9cee0d4e24a161bef06ae_Sudha%20color%20(home%20page).png"}/>
+  </div>
+</div>
 
 
 
@@ -32,7 +67,7 @@ const Donate = () => {
                 <div  class="inner-wrapper-hero sad"><div class="payemnt-wrapper"><div data-w-id="2d335d07-5ffe-574b-2c97-328d886917e2" class="text-block-2">Choose Your Donation Amount</div><div class="div-block-6"><div data-w-id="2d335d07-5ffe-574b-2c97-328d886917e5" class="div-block-4 amounts" style={{ opacity: 1, transform: 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)', transformStyle: 'preserve-3d' }}>500</div><div data-w-id="2d335d07-5ffe-574b-2c97-328d886917e7" class="div-block-4 amounts" style={{ opacity: 1, transform: 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)', transformStyle: 'preserve-3d' }}>1000</div><div data-w-id="2d335d07-5ffe-574b-2c97-328d886917e9" class="div-block-4 amounts" style={{ opacity: 1, transform: 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)', transformStyle: 'preserve-3d' }}>5000</div></div><div class="div-block-6"><div data-w-id="2d335d07-5ffe-574b-2c97-328d886917ec" class="div-block-4 amounts" style={{ opacity: 1, transform: 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)', transformStyle: 'preserve-3d' }}>10000</div><div data-w-id="2d335d07-5ffe-574b-2c97-328d886917ee" class="div-block-4 amounts other" style={{ opacity: 1, transform: 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)', transformStyle: 'preserve-3d' }}>OTHER AMOUNT</div></div><div class="div-block-6"><div data-w-id="2d335d07-5ffe-574b-2c97-328d886917f1" class="div-block-4 amounts other button saf" style={{ opacity: 1, transform: 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)', transformStyle: 'preserve-3d' }}>MAKE A DIFFERENCE NOW!</div></div></div></div>
 
                 </div> 
-
+{/* 
                 <div class="cards">
                     <article class="plan [ card ]">
                         <div class="inner">
@@ -73,9 +108,9 @@ const Donate = () => {
                                     <span><strong>Make independent </strong></span>
                                 </li>
                             </ul>
-                            {/* <button class="dn_button">
+                             <button class="dn_button">
                                 Choose plan
-                            </button> */}
+                            </button> 
                         </div>
                     </article>
 
@@ -83,13 +118,13 @@ const Donate = () => {
                         <span class="tag">Healthcare</span>
                         <h2 class="title">Empower Lives, Transform Tomorrow</h2>
                         <p class="info"> With surgeries, treatments, and ongoing healthcare, you bring hope and healing to disabled individuals. Your generosity paves the way for brighter tomorrows. Join us in making a meaningful impact on the journey to wellness. </p>
-                        {/* <button class="dn_button">
+                         <button class="dn_button">
                             <span>Learn more</span>
                             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="none">
                                 <path d="M0 0h24v24H0V0z" fill="none" />
                                 <path d="M16.01 11H4v2h12.01v3L20 12l-3.99-4v3z" fill="currentColor" />
                             </svg>
-                        </button> */}
+                        </button> 
                     </article>
                     <article class="plan [ card ]">
                         <div class="inner">
@@ -101,7 +136,6 @@ const Donate = () => {
 
 
 
-                                    {/* <small>/ m</small> */}
                                 </span>
                             </span>
                             <h2 class="title">Raise Scholarships</h2>
@@ -135,12 +169,12 @@ const Donate = () => {
                                     <span><strong>Break Barriers</strong></span>
                                 </li>
                             </ul>
-                            {/* <button class="dn_button">
+                             <button class="dn_button">
                                 Choose plan
-                            </button> */}
+                            </button> 
                         </div>
                     </article>
-                    {/* <article class="information [ card ]">
+                     <article class="information [ card ]">
     <h2 class="title">The only tool you need to schedule your day</h2>
     <p class="info">Always keep updated with this simple tool on the go, when and where ever you need.</p>
     <dl class="details dl">
@@ -153,16 +187,137 @@ const Donate = () => {
             <dd>4.5K</dd>
         </div>
     </dl>
-</article> */}
+</article> 
                 </div>
-
-                {/* <img className='donatebanner' width={'820px'} height={'300px'} src={donate_banner} alt="" /> */}
-
+                
+            */}
+               
 
                 
 
             </div>
-            <Widgets />
+
+
+
+            {/* <Widgets /> */}
+
+            <div className="widgets">
+    
+
+    <div className="widgets__widgetContainer">
+
+
+      <div class="cards">
+                    <article class="plan [ card ]">
+                        <div class="inner">
+
+                            <span class="pricing">
+                                <span>
+                                    <img width={'20px'} height={'20px'} src="https://www.flaticon.com/free-icons/book" alt="" />
+                                </span>
+                            </span>
+                            <h2 class="title">Education and Training</h2>
+                            <p class="info">This plan is for those who have a team already and running a large business.</p>
+                            <ul class="features">
+                                <li class="li">
+                                    <span class="icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                            <path fill="none" d="M0 0h24v24H0z" />
+                                            <path d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z" fill="currentColor" />
+                                        </svg>
+                                    </span>
+                                    <span><strong>Vocational Training</strong></span>
+                                </li>
+                                <li class="li">
+                                    <span class="icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                            <path fill="none" d="M0 0h24v24H0z" />
+                                            <path d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z" fill="currentColor" />
+                                        </svg>
+                                    </span>
+                                    <span><strong>Advocate hiring</strong></span>
+                                </li>
+                                <li class="li">
+                                    <span class="icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                            <path fill="none" d="M0 0h24v24H0z" />
+                                            <path d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z" fill="currentColor" />
+                                        </svg>
+                                    </span>
+                                    <span><strong>Make independent </strong></span>
+                                </li>
+                            </ul>
+                          
+                        </div>
+                    </article>
+
+                    {/* <article class="information [ card ]">
+                        <span class="tag">Healthcare</span>
+                        <h2 class="title">Empower Lives, Transform Tomorrow</h2>
+                        <p class="info"> With surgeries, treatments, and ongoing healthcare, you bring hope and healing to disabled individuals. Your generosity paves the way for brighter tomorrows. Join us in making a meaningful impact on the journey to wellness. </p>
+                        
+                    </article> */}
+                    <article class="plan [ card ]">
+                        <div class="inner">
+
+                            <span class="pricing">
+                                <span>
+
+                                    <img width={'20px'} height={'20px'} src="https://www.flaticon.com/free-icons/book" alt="" />
+
+
+                                </span>
+                            </span>
+                            <h2 class="title">Raise Scholarships</h2>
+                            <p class="info">Financial barriers can often hinder individuals with disabilities </p>
+                            <ul class="features">
+                                <li class="li">
+                                    <span class="icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                            <path fill="none" d="M0 0h24v24H0z" />
+                                            <path d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z" fill="currentColor" />
+                                        </svg>
+                                    </span>
+                                    <span><strong>Empower Through Education</strong></span>
+                                </li>
+                                <li class="li">
+                                    <span class="icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                            <path fill="none" d="M0 0h24v24H0z" />
+                                            <path d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z" fill="currentColor" />
+                                        </svg>
+                                    </span>
+                                    <span><strong>Enable Skills Acquisition</strong></span>
+                                </li>
+                                <li class="li">
+                                    <span class="icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                            <path fill="none" d="M0 0h24v24H0z" />
+                                            <path d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z" fill="currentColor" />
+                                        </svg>
+                                    </span>
+                                    <span><strong>Break Barriers</strong></span>
+                                </li>
+                            </ul>
+                            
+                        </div>
+                    </article>
+                    
+                </div>
+
+
+
+
+
+
+
+
+      
+    </div>
+  </div>
+
+
+
         </div>
     )
 }
