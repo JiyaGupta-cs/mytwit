6de661b4aba25a9c8f425142a980dbd27d7b77f0
Priyadarshini <priyadarshini.roy03@gmail.com>
2024-01-17 00:13:42 +0530

    added condition

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,34 +1,55 @@
-import React, { useState } from 'react'
-import './TweetBox.css'
-import {db} from './firebase'
+import React, { useState } from "react";
+import "./TweetBox.css";
+import { db } from "./firebase";
 
 const TweetBox = () => {
-  const[tweetMessage,setTweetMessage]   = useState("");
-  const[tweetImage,setTweetImage] = useState("");
-  const sendTweet = e =>{
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+  const sendTweet = (e) => {
     e.preventDefault();
-    db.collection('posts').add({
-      displayName:'Julie Jain',
-      username:'@juliejain',
-      verified:true,
-      text:tweetMessage,
-      image:tweetImage,
-      avatar:"https://static.vecteezy.com/system/resources/thumbnails/002/002/403/small/man-with-beard-avatar-character-isolated-icon-free-vector.jpg"
-    })
+    if (tweetMessage == "") {
+      console.log("Post cannot be empty.");
+      return;
+    }
+    db.collection("posts").add({
+      displayName: "Julie Jain",
+      username: "@juliejain",
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar:
+        "https://static.vecteezy.com/system/resources/thumbnails/002/002/403/small/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
+    });
   };
   return (
-    <div className='tweetBox'>
+    <div className="tweetBox">
       <form>
-      <div className="tweetBox_input">
-        <img src="" alt="" />
-        <input onChange={e => setTweetMessage(e.target.value)} value={tweetMessage} placeholder="What's happening?" type="text" />
-        
-      </div>
-      <input onChange={e => setTweetImage(e.target.value)} value={tweetImage} className='tweetBox_imageInput' placeholder="Optional: Enter image URL" type="text" />
-      <button onClick={sendTweet} type='submit' className='tweetBox_tweetButton'>Post</button>
-    </form>      
+        <div className="tweetBox_input">
+          <img src="" alt="" />
+          <input
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+            placeholder="What's happening?"
+            type="text"
+          />
+        </div>
+        <input
+          onChange={(e) => setTweetImage(e.target.value)}
+          value={tweetImage}
+          className="tweetBox_imageInput"
+          placeholder="Optional: Enter image URL"
+          type="text"
+        />
+        <button
+          onClick={sendTweet}
+          type="submit"
+          className="tweetBox_tweetButton"
+        >
+          Post
+        </button>
+      </form>
     </div>
-  )   
-}
-      
-export default TweetBox
+  );
+};
+
+export default TweetBox;
