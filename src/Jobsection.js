9abb39ae960a4ebe76b45a3f5b07461ed9a25306
jiya10208 <jiya102082004@gmail.com>
2024-01-28 16:33:59 +0530

    adding a title.

--- src/Jobsection.js
+++ src/Jobsection.js
@@ -1,47 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import './Feed.css'
+import React, { useEffect, useState } from "react";
+import "./Feed.css";
 // import TweetBox from './TweetBox'
 // import Post from './Post'
-import Job from './Job'
-import {db} from './firebase'
-import './Jobsection.css'
+import Job from "./Job";
+import { db } from "./firebase";
+import "./Jobsection.css";
 
 const Jobsection = () => {
-  const[jobs,setJobs]=useState([]);
+  const [jobs, setJobs] = useState([]);
 
-  useEffect(()=>{
-    db.collection('jobs').onSnapshot(snapshot=>(
-      setJobs(snapshot.docs.map(doc => doc.data()))
-    ))
-  },[]);
+  useEffect(() => {
+    db.collection("jobs").onSnapshot((snapshot) =>
+      setJobs(snapshot.docs.map((doc) => doc.data()))
+    );
+  }, []);
 
   return (
-    <div className='jobsection'>
-      {/* Header */}
-      {/* <div className="feed_header">
+    <div className="job-component">
+      <h1 className="job_title"> %Job title% </h1>
+      <div className="jobsection">
+        {/* Header */}
+        {/* <div className="feed_header">
       <h2>Home</h2>
       </div> */}
-      
-
-      {jobs.map(job => (
-        
-      <Job
-displayName={job.displayName}
-userName={job.username}
-verified={job.verified}
-text={job.text}
-avatar={job.avatar}
-image={job.image}
-location={job.location}
-salary={job.salary}
-type={job.type}
-title={job.title}
-      />
-      
-      ))}
-
+        {jobs.map((job) => (
+          <Job
+            displayName={job.displayName}
+            userName={job.username}
+            verified={job.verified}
+            text={job.text}
+            avatar={job.avatar}
+            image={job.image}
+            location={job.location}
+            salary={job.salary}
+            type={job.type}
+            title={job.title}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Jobsection
+export default Jobsection;
