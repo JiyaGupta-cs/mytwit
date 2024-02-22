b384ea0e79bab5385de1a1e8337fd3786d8503a4
Ramith Kulal <ramithnr1234@gmail.com>
2024-02-22 20:57:58 +0530

    Update Feed.js

--- src/Feed.js
+++ src/Feed.js
@@ -43,21 +43,22 @@ const Feed = () => {
   };
 
   const arrowColor = colorMode === 'dark' ? 'white' : 'black';
-  const buttonStyle = {
-    position: 'fixed',
-    bottom: showGoToTop ? '20px' : '-100px', // Show/hide based on scroll position
-    right: '20px',
-    backgroundColor: 'yellow',
-    color: 'black',
-    padding: '8px',
-    borderRadius: '50%',
-    height: '40px',
-    width: '40px',
-    fontSize: '27px',
-    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-    transition: 'bottom 0.3s ease-in-out',
-    marginRight: '440px',
-  };
+ const buttonStyle = {
+  position: 'fixed',
+  bottom: showGoToTop ? '20px' : '-100px', // Show/hide based on scroll position
+  right: '20px',
+  backgroundColor: '#3685fb', // Specific color code for the background
+  color: '#0d213f', // Specific color code for the text
+  padding: '8px',
+  borderRadius: '50%',
+  height: '40px',
+  width: '40px',
+  fontSize: '27px',
+  boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+  transition: 'bottom 0.3s ease-in-out',
+  marginRight: '440px',
+};
+
 
   return (
     <div className='feed' ref={feedRef}>
